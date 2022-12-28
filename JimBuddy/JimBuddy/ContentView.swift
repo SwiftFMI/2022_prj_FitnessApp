9ec8f8c8ef925608378f32db5930a8260543554b
Simeon Hristov <simeon.hristov@paysafe.com>
2022-12-28 16:14:13 +0200

    Create empty project

---
+++ JimBuddy/JimBuddy/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  JimBuddy
+//
+//  Created by Simeon Hristov on 28.12.22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ JimBuddy/JimBuddy/JimBuddyApp.swift
@@ -0,0 +1,17 @@
+//
+//  JimBuddyApp.swift
+//  JimBuddy
+//
+//  Created by Simeon Hristov on 28.12.22.
+//
+
+import SwiftUI
+
+@main
+struct JimBuddyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
